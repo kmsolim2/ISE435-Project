2337875b714d1cca59399140c1cd769902feffb4
kmsolim2 <74207171+kmsolim2@users.noreply.github.com>
2020-11-09 19:28:39 -0500

    Add files via upload

--- ISE435_F2020_Project_Karl Soliman.py
+++ ISE435_F2020_Project_Karl Soliman.py
@@ -5,19 +5,15 @@ Created on Sun Nov  8 19:50:30 2020
 @author: Karl Angelo Soliman
 """
 
-## Creating a dataframe from expenses data 
+#%% Creates a dataframe from expenses data 
 
-import zipfile as zp
-import pandas as pd
-import datetime as dt
-import matplotlib.pyplot as plt
-import numpy as np
+import zipfile as zp, pandas as pd, matplotlib.pyplot as plt, datetime as dt
 
 path = 'Expenses.zip' # expenses are stored by year in a zip file 
 zipfile = zp.ZipFile(path, 'r')
 files = zipfile.namelist()
 
-allDF = pd.DataFrame() # creates a dataframe for the data from all years
+allDF = pd.DataFrame() # creates a dataframe to hold the data from all years
 
 for file in files:
     if file.endswith('.csv'):
@@ -26,115 +22,118 @@ for file in files:
 
 allDF['Expense'] = allDF['Expense'].replace('[$,]', '', regex=True).astype(float) # converts data in 'Expense' column from string to float 
 
-## Creates individual dataframes for each year 
+#%% Function - finds the monthly expenses for a given year 
 
-year2017 = allDF[allDF['Date'].dt.year == 2017]
-year2018 = allDF[allDF['Date'].dt.year == 2018] 
-year2019 = allDF[allDF['Date'].dt.year == 2019] 
-year2020 = allDF[allDF['Date'].dt.year == 2020] 
-
-## Separates data by month
-
-months = {1:'Jan',
-          2:'Feb',
-          3:'Mar',
-          4:'Apr',
-          5:'May',
-          6:'Jun',
-          7:'Jul',
-          8:'Aug',
-          9:'Sep',
-          10:'Oct',
-          11:'Nov',
-          12:'Dec'}
-
-listMonths = [months[k] for k in months]
-
-monthly2017 = []
-monthly2018 = []
-monthly2019 = []
-monthly2020 = []
-
-# Calculates monthly expenses for each year 
-
-for k in months :
-    monthlyDF = year2017[year2017['Date'].dt.month == k]
-    monthly2017.append(monthlyDF['Expense'].sum().round(2))
+def byMonth(y): # 'yr' is the year to be analyzed
     
-    monthlyDF = year2018[year2018['Date'].dt.month == k]
-    monthly2018.append(monthlyDF['Expense'].sum().round(2))
+    months = {1:'Jan' , 2:'Feb' , 3:'Mar' , 4:'Apr', 5:'May' , 6:'Jun' ,
+              7:'Jul' , 8:'Aug' , 9:'Sep' , 10:'Oct' , 11:'Nov' , 12:'Dec'}
     
-    monthlyDF = year2019[year2019['Date'].dt.month == k]
-    monthly2019.append(monthlyDF['Expense'].sum().round(2))
+    yearly = allDF[allDF['Date'].dt.year == y]
+    monthly = []
     
-    monthlyDF = year2020[year2020['Date'].dt.month == k]
-    monthly2020.append(monthlyDF['Expense'].sum().round(2))    
+    for k in months:
+        monthlyDF = yearly[yearly['Date'].dt.month == k]
+        monthly.append(monthlyDF['Expense'].sum().round(2))
+    
+    return monthly
 
-# Calculates total expenses  and yearly expenses
+#%% Function - finds the monthly expenses for a given year 
 
-total = allDF['Expense'].sum().round(2) # total expenses over all years 
-print('Total :', total)
+def byYear(y): # 'yr' is the year to be analyzed
+    yearly = allDF[allDF['Date'].dt.year == y]
+    total = yearly['Expense'].sum().round(2)
+    
+    return total
 
-total2017 = year2017['Expense'].sum().round(2) # finds the total expenses for a given year
-total2018 = year2018['Expense'].sum().round(2)
-total2019 = year2019['Expense'].sum().round(2)
-total2020 = year2020['Expense'].sum().round(2)
+#%% Function - finds the monthly expenses for a given year 
 
-yrTotal = [total2017, total2018, total2019, total2020]
-yr = [2017, 2018, 2019, 2020]
+def catYear(y): # 'yr' is the year to be analyzed
+    yearly = allDF[allDF['Date'].dt.year == y]
+    topCats = yearly['Category'].value_counts().head(3)
+    
+    return topCats.to_string()
 
-for y, t in zip(yr, yrTotal):
-    print(f'{y} : {t}')
+#%% Function - graphs the monthly data for a given year 
 
-## Finds top 3 categories
+def plotData(monthly, y): # 'data' should be the list of monthly expenses, 'yr' is the year of the monthly list
 
-topCats = allDF['Category'].value_counts().head(3)
-print(topCats)
+    months = {1:'Jan' , 2:'Feb' , 3:'Mar' , 4:'Apr', 5:'May' , 6:'Jun' ,
+              7:'Jul' , 8:'Aug' , 9:'Sep' , 10:'Oct' , 11:'Nov' , 12:'Dec'}
 
-## Graph yearly data
+    listMonths = [months[k] for k in months]
+    
+    dataGraph = plt.figure()
+    plt.bar(listMonths, monthly)
+    plt.title(f'Expenses ({y})')
+    plt.xlabel('Month')
+    plt.ylabel('Amount Spent ($)')
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.show()
+    
+    return dataGraph
 
-fig1 = plt.figure(1)
-plt.bar(listMonths, monthly2017)
-plt.title('Expenses (2017)')
-plt.xlabel('Month')
-plt.ylabel('Amount Spent ($)')
-plt.grid(True)
-plt.xticks(rotation=45)
+#%% PySimpleGUI
 
-fig2 = plt.figure(2)
-plt.bar(listMonths, monthly2018, color='orange')
-plt.title('Expenses (2018)')
-plt.xlabel('Month')
-plt.ylabel('Amount Spent ($)')
-plt.grid(True)
-plt.xticks(rotation=45)
+options = ('2017', '2018', '2019', '2020', 'All')
 
-fig3 = plt.figure(3)
-plt.bar(listMonths, monthly2019, color='green')
-plt.title('Expenses (2019)')
-plt.xlabel('Month')
-plt.ylabel('Amount Spent ($)')
-plt.grid(True)
-plt.xticks(rotation=45)
+import PySimpleGUI as sg
 
-fig4 = plt.figure(4)
-plt.bar(listMonths, monthly2020, color='red')
-plt.title('Expenses (2020)')
-plt.xlabel('Month')
-plt.ylabel('Amount Spent ($)')
-plt.grid(True)
-plt.xticks(rotation=45)
+sg.theme('SystemDefaultForReal')
 
-fig5 = plt.figure(5) # creates a stacked bar chart
-plt.bar(listMonths, monthly2017)
-plt.bar(listMonths, monthly2018, bottom=monthly2017) 
-plt.bar(listMonths, monthly2019, bottom=[i + j for i, j in zip(monthly2017, monthly2018)]) # combines data frome 2017 and 2018
-plt.bar(listMonths, monthly2020, bottom=[i + j + k for i, j, k in zip(monthly2017, monthly2018, monthly2019)]) # combines data from 2017, 2018, and 2019
-plt.title('Expenses (All Years)')
-plt.xlabel('Month')
-plt.ylabel('Amount Spent ($)')
-plt.grid(True)
-plt.xticks(rotation=45)
-plt.legend([2017, 2018, 2019, 2020])
+layout = [[sg.Text('Choose an Option:\t'), sg.Combo(options, size=(10,1)), sg.Button('Analyze'), sg.Button('Cancel')],
+          [sg.Text('Total'), sg.Text(key='Total', relief='sunken', size=(10,1), background_color='white'), sg.Text('Top Categories'), sg.Multiline(key='Top', size=(12,3), background_color='white')]]
 
+window = sg.Window('Expenses Analysis Tool', layout)
+
+while True:  # Event Loop
+    event, values = window.read()
+    print(event, values)
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
+    if event == 'Analyze' and event != '':
+        if values[0] in ['2017', '2018', '2019', '2020']:
+            y = int(values[0])
             
+            monthly = byMonth(y)
+            total = byYear(y)
+            topCat = catYear(y)
+            
+            window['Total'].update(total)
+            window['Top'].update(topCat)
+            
+            fig1 = plotData(monthly, y)
+            
+        else: 
+            total = allDF['Expense'].sum().round(2) # total expenses over all years 
+            topCat = allDF['Category'].value_counts().head(3)
+            
+            window['Total'].update(total)
+            window['Top'].update(topCat)
+            
+            ################ GRAPHING #################\
+                
+            months = {1:'Jan' , 2:'Feb' , 3:'Mar' , 4:'Apr', 5:'May' , 6:'Jun' ,
+                      7:'Jul' , 8:'Aug' , 9:'Sep' , 10:'Oct' , 11:'Nov' , 12:'Dec'}
+            
+            listMonths = [months[k] for k in months]
+            
+            monthly2017 = byMonth(int('2017'))
+            monthly2018 = byMonth(int('2018'))
+            monthly2019 = byMonth(int('2019'))
+            monthly2020 = byMonth(int('2020'))
+            
+            plt.figure()
+            plt.bar(listMonths, monthly2017)
+            plt.bar(listMonths, monthly2018, bottom=monthly2017) 
+            plt.bar(listMonths, monthly2019, bottom=[i + j for i, j in zip(monthly2017, monthly2018)]) # combines data frome 2017 and 2018
+            plt.bar(listMonths, monthly2020, bottom=[i + j + k for i, j, k in zip(monthly2017, monthly2018, monthly2019)]) # combines data from 2017, 2018, and 2019
+            plt.title('Expenses (All Years)')
+            plt.xlabel('Month')
+            plt.ylabel('Amount Spent ($)')
+            plt.grid(True)
+            plt.xticks(rotation=45)
+            plt.legend([2017, 2018, 2019, 2020])
+            
+window.close()
